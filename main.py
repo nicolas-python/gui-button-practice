72730293ab26781ddcd7a0182d757f579afbce09
Nicolas <nicolasmader001@gmail.com>
2026-04-08 15:13:35 +0200

    add button height and width

--- main.py
+++ main.py
@@ -7,10 +7,11 @@ root = tkinter.Tk()                 #erstellt das hauptfenster
 root.title("Button Test")           #fenstertitel
 root.geometry("400x300")            #fenstergröße
 
-button1= tkinter.Button(root, text="Button 1",command=click)   #erstellt button
-button1.pack()                                                  #zeigt den button an
+button1= tkinter.Button(root, text="Button 1",command=click,width=20 , height=3 )   #erstellt button
+button1.pack()                                                                      #zeigt den button an
 
-button2= tkinter.Button(root, text="Button 2",command=click)   #command = sagt diese funktion ausführen
-button2.pack()
+button2= tkinter.Button(root, text="Button 2",command=click, width=10 , height=20)   #command = sagt diese funktion ausführen
+button2.pack()                                                                       #width=Breite in zeichen
+                                                                                     #height=Höhe in zeilen
 
 root.mainloop()                     #startet die schleife

614021526e187facce4c1bbe71e27847deb1b9e9
Nicolas <nicolasmader001@gmail.com>
2026-04-08 15:41:49 +0200

    add change text

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import tkinter                      # importiert tkinter
 def click():
     print("clicked")
 
+def change_text():
+    button.config(text="Geklickt!")
+
 root = tkinter.Tk()                 #erstellt das hauptfenster
 root.title("Button Test")           #fenstertitel
 root.geometry("600x400")            #fenstergröße
@@ -20,12 +23,15 @@ root.geometry("600x400")            #fenstergröße
 #button3.grid(row=0, column=5, padx=30, pady=10)                           #grid =Positionierung im Raster mit meinen befehle
                                                                     #padx/pady Abstand vom rand in x/y richtung (vertikaler Abstand, wirkt nur, wenn Zeilenhöhe groß genug ist)
                                                                     #row=Zeile column=spalte
-#Knöpfe
+#Knöpfe verschieben/bewegen
 button4= tkinter.Button(root, text="Button 4",command=click)         #pack und grid dürfen im selben Eltern-Fenster verwendet werden
 button4.pack(expand=True,anchor="center")                            #anchor=die ausrichtung innerhalb der box (optionen=n,s,e,w,center north south...
 
 button5= tkinter.Button(root, text="Button 5", command=click)         #pack und grid dürfen im selben Eltern-Fenster verwendet werden
 button5.pack(expand=True,anchor="sw")                                  #expand=True =pack dehnt sich und verteil den freien raum
 
+#knöpfe Text ändern
+button = tkinter.Button(root, text="Klick mich", command=change_text)
+button.pack(expand=True)
 
 root.mainloop()                     #startet die schleife
